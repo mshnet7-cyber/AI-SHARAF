bccbb4054099b1dbed4501d85beddd33b57bc7fa
mshnet7-cyber <msh.net7@gmail.com>
2026-09-01 03:06:01 +0400

    Fix JVM target compatibility for Android build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,15 @@ android {
         versionName = "2.0"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildFeatures { compose = true }
 
     packaging {
